d221514ba1ade30cb3b5dba0efe6933fe370712a
rifatNur <2306853@edu.karelia.fi>
2026-01-29 23:45:21 +0600

    fix error for jwt

--- middleware/verifytoken.js
+++ middleware/verifytoken.js
@@ -1,35 +1,36 @@
-import jwt from 'jsonwebtoken';
-import 'dotenv/config';
-import { logonUsers } from '../db/db.js'
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { logonUsers } from "../db/db.js";
+dotenv.config();
 
 const secret = process.env.MY_SECRET_KEY;
 
 // middleware/verifyToken
 export const verifyToken = (req, res, next) => {
-  if(req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
 
-  const authHeader = req.header('Authorization');
-  
-  if (!authHeader?.toLowerCase().startsWith('bearer ')) {
-    return res.status(401).json({ error: 'Unauthorized' });
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader?.toLowerCase().startsWith("bearer ")) {
+    return res.status(401).json({ error: "Unauthorized" });
   }
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
 
   try {
     const decodedToken = jwt.verify(token, secret, {
-      algorithms: ['HS256']
+      algorithms: ["HS256"],
     });
-  
-    const user = logonUsers.get(decodedToken.username)
+
+    const user = logonUsers.get(decodedToken.username);
 
     if (!user || user.token !== token) {
-      return res.status(401).json({ error: 'Unauthorized' });
+      return res.status(401).json({ error: "Unauthorized" });
     }
-    next()
+    next();
   } catch (err) {
-      return res.status(401).json({ error: 'Unauthorized' });
-  }    
+    return res.status(401).json({ error: "Unauthorized" });
+  }
 };
